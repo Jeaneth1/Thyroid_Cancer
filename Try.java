1903ea2efcdae4c027370c4ae6aa4b2f450497c5
Anjiya Shrestha <anjiya2021@gmail.com>
2025-05-04 13:08:59 -0400

    TRY hello world

---
+++ Try.java
@@ -0,0 +1,9 @@
+    public class Try {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!how are you?");
+            System.out.println("Hello, World!");
+            System.out.println("how are you?");
+        }
+   
+
+  }
